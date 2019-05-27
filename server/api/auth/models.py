916e5867ba5be74b9a142606a2c8e8d7adca8f41
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-27 13:03:17 +0900

    fix: use strict_slashes

--- server/api/auth/models.py
+++ server/api/auth/models.py
@@ -1,4 +1,15 @@
 login_model = {
     'token': str,
-    'refresh_token': str
+    'refresh_token': str,
+    'user': {
+        'idx': int,
+        'name': str,
+        'grade': str,
+        'klass': str,
+        'number': str,
+        'serial': str,
+        'photo': str,
+        'email': str,
+        'user_type': str
+    }
 }
