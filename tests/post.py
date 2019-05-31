62f67b9d474169501d9f4ca1ad8523a52f93d707
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-31 23:14:32 +0900

    fix: delete unnecessary dimigoin request

--- tests/post.py
+++ tests/post.py
@@ -4,15 +4,17 @@ import json
 import sys
 import requests_async as requests
 
-async def test():
+async def get_token():
     with open(os.path.join(sys.path[0], 'secret.json')) as secret_file:
         user = json.load(secret_file)
-        print(user)
-    res = await requests.post('http://localhost:5000/auth/login', json={
+    return json.loads(await requests.post('http://localhost:5000/auth/login', json={
         'id': user['id'],
         'password': user['password']
-    })
-    print(res.text)
+    }).text)['token']
+
+async def test():
+    token = await get_token()
+    print(token)
 
 if __name__ == '__main__':
     asyncio.run(test())
