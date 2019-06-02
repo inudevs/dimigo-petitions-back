7841ed62c360a6d6f387ba5db904657e5797e838
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-06-03 08:47:26 +0900

    refactor(test): use pytest-sanic

--- server/__init__.py
+++ server/__init__.py
@@ -9,17 +9,18 @@ def create_app():
     _app.config.from_object(DevConfig)
     JWTManager(_app)
     CORS(_app)
+
+    _app.blueprint(swagger_blueprint)
+
+    from motor.motor_asyncio import AsyncIOMotorClient
+    @_app.listener('before_server_start')
+    def init(app, loop):
+        _app.db = AsyncIOMotorClient(_app.config.MONGO_URI)[_app.config.MONGO_DB]
+
+    from server.api import api
+    _app.blueprint(api)
+
     return _app
 
 
 app = create_app()
-app.blueprint(swagger_blueprint)
-
-from motor.motor_asyncio import AsyncIOMotorClient
-
-@app.listener('before_server_start')
-def init(app, loop):
-    app.db = AsyncIOMotorClient(app.config.MONGO_URI)[app.config.MONGO_DB]
-
-from server.api import api
-app.blueprint(api)
